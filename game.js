eb21096182713e2dda8710d9a57b243c8d3f8278
Agustin <agsutinromano10@yahoo.com>
2022-05-26 14:50:29 -0400

    I have added some changed.

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 let playerScore = 0;
 let computerScore = 0;
+const buttons = document.querySelectorAll("button");
 
 function game() {
   for (let i = 1; i <= 5; i++) playRound(i);
@@ -10,27 +11,51 @@ function computerPlay() {
   return options[Math.floor(Math.random() * options.length)];
 }
 
-const playerSelection = window
-  .prompt("rock, paper, or scissors? ")
-  .toLowerCase();
-console.log(`You threw ${playerSelection}`);
-const computerSelection = computerPlay().toLowerCase();
-console.log(`Computer threw ${computerSelection}`);
-// console.log(playRound(playerSelection, computerSelection));
-console.log(`Player Score: ${playerScore}`, `Computer Score: ${computerScore}`);
+let computerSelection = computerPlay();
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+  let computerSelection = computerPlay();
+
   if (computerSelection == playerSelection) {
-    return "Tie try again.";
+    return `Tie try again. You thre ${playerSelection} and the computer threw ${computerSelection}`;
   } else if (
     (computerSelection == "rock" && playerSelection == "scissors") ||
     (computerSelection == "paper" && playerSelection == "rock") ||
     (computerSelection == "scissors" && playerSelection == "paper")
   ) {
     computerScore += 1;
-    return "You lost. The computer is smater.";
+    return `You lost. The computer threw ${computerSelection}.`;
   } else {
     playerScore += 1;
-    return "You won! Better than a machine I see.";
+    return `You won! ${playerSelection} beats ${computerSelection}`;
   }
 }
+
+game();
+
+//Rock button
+const rockBtn = document.querySelector(".rock-btn");
+rockBtn.addEventListener("click", () => {
+  playerSelection.value;
+  console.log("rock");
+});
+
+//Paper buttn
+const paperBtn = document.querySelector(".paper-btn");
+paperBtn.addEventListener("click", () => {
+  playRound();
+  console.log("paper");
+});
+
+//Scissors button
+const scissorsBtn = document.querySelector(".scissors-btn");
+scissorsBtn.addEventListener("click", () => {
+  playRound();
+  console.log("scissors");
+});
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    playRound(button.value);
+  });
+});
